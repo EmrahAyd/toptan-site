cc6e4d6f7c7a2bea5218348fc1f246caa5c658cb
Emrah AYDIN <91917229+EmrahAyd@users.noreply.github.com>
2022-05-24 12:36:51 +0300

    langFlag

--- app.js
+++ app.js
@@ -48,9 +48,141 @@ function autoChangeSlide() {
   }%)`;
   leftSlide.style.transform = `translateY(-${currentSlide * 100}%)`;
 }
+document.getElementById('tur').onclick = 
+function turk(){console.log("turk");
+document.querySelector(".cev1").innerHTML = "Anasayfa";
+document.querySelector(".cev2").innerHTML = "Link";
+document.querySelector(".cev3").innerHTML = "Dil Seçenekleri";
+document.querySelector(".cev4").innerHTML = "Kapalı";
+document.querySelector(".cev5").innerHTML = "Ara";
+document.querySelector(".cev6").innerHTML = "HARİKA";
+document.querySelector(".cev7").innerHTML = "Buraya Birşeyler Yazılır.";
+document.querySelector(".cev8").innerHTML = "awsome altı";
+document.querySelector(".cev9").innerHTML = "awsome";
+document.querySelector(".cev10").innerHTML = "awsome";
+document.querySelector(".cev11").innerHTML = "awsome";
+document.querySelector(".cev12").innerHTML = "Hemen Al!";
+document.querySelector(".cev13").innerHTML = "En Moda Elbiseler";
+document.querySelector(".cev14").innerHTML = "Hemen Al!";
+document.querySelector(".cev15").innerHTML = "Modanı yansıt.";
+document.querySelector(".cev16").innerHTML = "T-shirt";
+document.querySelector(".cev17").innerHTML = "Yazın vazgeçilmezi.";
+document.querySelector(".cev18").innerHTML = "Elbiseler";
+document.querySelector(".cev19").innerHTML = "Hem rahat, hem şık.";
+document.querySelector(".cev20").innerHTML = "Spor";
+document.querySelector(".cev21").innerHTML = "Tarz takımlar.";
+document.querySelector(".cev22").innerHTML = "Bluz";
+document.querySelector(".cev23").innerHTML = "Herzaman çok iyi.";
+document.querySelector(".cev24").innerHTML = "Abiye";
+document.querySelector(".cev25").innerHTML = "İster gece, ister gündüz.";
+document.querySelector(".cev26").innerHTML = "Hakkımızda";
+document.querySelector(".cev27").innerHTML = "ABD, Avrupa veya diğer 100'den fazla ülkede En Ucuz ve En Son Moda Kadın Giyim ürünlerini çevrimiçi toptan satış yapmak ister misiniz? İster bir butik mağaza, ister çevrimiçi perakendeci olun, klasikten modayı takip eden tarzlara, resmi işlerden gündelik tarzlara, Janes'de 50.000'den fazla benzersiz ve modaya uygun bayan giyim tarzı, toplu satın alma için ilk tercihiniz olacak! En iyi satıcı olarak, her mevsime veya duruma uygun en yeni şık kadın kıyafetlerini toptan satışa sunuyoruz. Yaza gelindiğinde kadın giyim pazarında en çok ne konuşuluyor? Seksi Mayo vazgeçilmezdir. Klasik Tek Parçalar veya Örtüler, Bikiniler, hatta Büyük Beden Mayoların tümü web sitelerimizde mevcuttur. Plaj Giysileri ve Mayoların tüm günlük güncellemeleri, trend stilleri, renkleri ve boyutları işletmenizi tatmin edecek. Çiçek Desenli Elbise veya Gömlek Elbise de yaz tutkusuna çok yakışıyor. Müşterinizin özel bir etkinliğe katılması gerekiyorsa, ona zarif bir Abiye veya şık bir Parti Elbisesi sunun. Bunun yanı sıra gündelik kadın giyimi de oldukça popülerdir. Hem Kabarık Kol Bayan Üstler, İş Butik Bluzlar, Baskı T-shirt, hem de Şeffaf Kumaş Bayan Spor Sütyen veya Şortlar, Janes'de en son stilleri toptan tek elden yapabilirsiniz.";
+document.querySelector(".cev28").innerHTML = "Kategoriler";
+document.querySelector(".cev29").innerHTML = "Bluz";
+document.querySelector(".cev30").innerHTML = "Pantolon";
+document.querySelector(".cev31").innerHTML = "Elbise";
+document.querySelector(".cev32").innerHTML = "Takım";
+document.querySelector(".cev33").innerHTML = "Bağlantılar";
+
+
+
+
+
+
+}
+
+document.getElementById('eng').onclick = 
+function engi(){console.log("eng");
+document.querySelector(".cev1").innerHTML = "Home";
+document.querySelector(".cev2").innerHTML = "Link";
+document.querySelector(".cev3").innerHTML = "Language";
+document.querySelector(".cev4").innerHTML = "Disabled";
+document.querySelector(".cev5").innerHTML = "Search";
+document.querySelector(".cev6").innerHTML = "AWESOME";
+document.querySelector(".cev7").innerHTML = "something";
+document.querySelector(".cev8").innerHTML = "awsome altı";
+document.querySelector(".cev9").innerHTML = "awsome";
+document.querySelector(".cev10").innerHTML = "awsome";
+document.querySelector(".cev11").innerHTML = "awsome";
+document.querySelector(".cev12").innerHTML = "Shop Now!";
+document.querySelector(".cev13").innerHTML = "Trend dress";
+document.querySelector(".cev14").innerHTML = "Shop Now!";
+document.querySelector(".cev15").innerHTML = "Reflect your style.";
+document.querySelector(".cev16").innerHTML = "T-shirt";
+document.querySelector(".cev17").innerHTML = "Summer essentials.";
+document.querySelector(".cev18").innerHTML = "Dresses";
+document.querySelector(".cev19").innerHTML = "Comfortable and stylish.";
+document.querySelector(".cev20").innerHTML = "Spor";
+document.querySelector(".cev21").innerHTML = "Style suits";
+document.querySelector(".cev22").innerHTML = "Blouse";
+document.querySelector(".cev23").innerHTML = "Always in style.";
+document.querySelector(".cev24").innerHTML = "Evening dresses";
+document.querySelector(".cev25").innerHTML = "Even in day.";
+document.querySelector(".cev26").innerHTML = "Abaout us";
+document.querySelector(".cev27").innerHTML = "Want to wholesale Cheapest and Latest Fashion Women's Clothing online in USA, Europe or other 100+ countries? Whether you're a boutique store or an online retailer, from classic to trendy, formal to casual, over 50,000 unique and trendy women's clothing styles at Janes will be your first choice for bulk buying! As a best seller, we wholesale the latest stylish women's clothing suitable for any season or occasion. When it comes to summer, what is the most talked about women's clothing market? Sexy Swimwear is indispensable. Classic One Pieces or Cover ups, Bikinis, even Plus Size Swimwear are all available on our websites. All daily updates of Beachwear and Swimwear, trending styles, colors and sizes will satisfy your business. Floral Patterned Dress or Shirt Dress is also very suitable for summer passion. If your client needs to attend a special event, offer them an elegant Evening Dress or an elegant Party Dress. In addition, casual women's clothing is also very popular. Both the Puffy Sleeve Women's Tops, Business Boutique Blouses, Print T-shirt, and Sheer Fabric Women's Sports Bra or Shorts, you can wholesale the latest styles at Janes one-stop.";
+document.querySelector(".cev28").innerHTML = "Categories";
+document.querySelector(".cev29").innerHTML = "Blouse";
+document.querySelector(".cev30").innerHTML = "Pants";
+document.querySelector(".cev31").innerHTML = "Dresses";
+document.querySelector(".cev32").innerHTML = "Spor";
+document.querySelector(".cev33").innerHTML = "Quick Links";
+
+
+
+
+
+
+}
+
 document.getElementById('arabi').onclick = 
 function changer(){console.log("arabi");
-document.querySelector(".info").innerHTML = "هل ترغب في بيع ملابس نسائية بأرخص وأحدث صيحات الموضة بالجملة عبر الإنترنت في الولايات المتحدة الأمريكية أو أوروبا أو أكثر من 100 دولة أخرى؟ سواء كنت متجرًا بوتيكًا أو تاجر تجزئة عبر الإنترنت ، من الأنماط الكلاسيكية إلى العصرية ، من الأعمال الرسمية إلى الأنماط غير الرسمية ، فإن FashionTIY سيكون لديها أكثر من 50000 من أنماط الملابس النسائية الفريدة والعصرية لتكون خيارك الأول للشراء بالجملة! كأفضل بائع ، نقوم بالجملة بأحدث الملابس النسائية الأنيقة المناسبة لأي موسم أو مناسبة. عندما يتعلق الأمر بالصيف ، ما هو أكثر ما يتم الحديث عنه في سوق الملابس النسائية؟ ملابس السباحة مثير لا غنى عنها. البيكينيات ، البيكينيات ، وحتى ملابس السباحة ذات المقاسات الكبيرة ، كلها متاحة على مواقعنا الإلكترونية. جميع التحديثات اليومية لملابس البحر وملابس السباحة والأنماط والألوان والأحجام الشائعة سترضي عملك. فستان مزخرف بالزهور أو فستان القميص مناسب أيضًا لشغف الصيف. إذا كان عميلك بحاجة إلى حضور مناسبة خاصة ، فقدم له فستان سهرة أنيقًا أو فستان حفلة أنيق. بالإضافة إلى ذلك ، تحظى الملابس النسائية غير الرسمية بشعبية كبيرة أيضًا. يمكنك بيع أحدث الأنماط في كل من بلوزات Puffy Sleeve النسائية ، وبلوزات Business Boutique ، والقميص المطبوع ، والصدرية أو الشورتات الرياضية النسائية من القماش الشفاف ، FashionTIY و NO MOQ. كما نقدم خيارات متنوعة لاتجاهات ملابس الأمومة والأحجام الكبيرة ومنتجات موسم الشتاء. ستعمل القمصان والسراويل النسائية ذات الحجم الزائد وسراويل الأمومة على تحديث مجموعة منتجات البيع بالتجزئة الخاصة بك. الدنيم بالجملة لا يمكن أن تفوت أيضا. سواء كنت تبحث عن بنطلون جينز أو شورت لطيف من الدنيم ، أو بدلات دينيم مثيرة أو فساتين وسترات مستوحاة من راعية البقر ، فمن المؤكد أن مجموعتنا الرائعة ستحصل على ما تبحث عنه بالجملة. من خلال التعاون مع الشركات المصنعة الممتازة ، تعد FashionTIY أفضل مورد بأفضل جودة واتجاهات بأرخص الأسعار ، وهم الموزعون الوحيدون لبيع الملابس النسائية بالجملة. سنتخذ رقابة أكثر صرامة على الجودة ، والتعبئة الماهرة ، والشحن السريع وخدمة العملاء في الوقت المناسب لضمان جودة الخدمة لأعمال بيع الملابس النسائية بالجملة";
-document.querySelector("#hemen").innerHTML = "خذ. فورا";
-document.querySelector("#hemen1").innerHTML = "خذ. فورا";}
+document.querySelector(".cev1").innerHTML = "الصفحة الرئيسية";
+document.querySelector(".cev2").innerHTML = "وصلة";
+document.querySelector(".cev3").innerHTML = "لغة";
+document.querySelector(".cev4").innerHTML = "معاق";
+document.querySelector(".cev5").innerHTML = "بحث";
+document.querySelector(".cev6").innerHTML = "مدهش";
+document.querySelector(".cev7").innerHTML = "شيئا ما";
+document.querySelector(".cev8").innerHTML = "awsome altı";
+document.querySelector(".cev9").innerHTML = "awsome";
+document.querySelector(".cev10").innerHTML = "awsome";
+document.querySelector(".cev11").innerHTML = "awsome";
+document.querySelector(".cev12").innerHTML = "تسوق الآن!";
+document.querySelector(".cev13").innerHTML = "فستان الاتجاه";
+document.querySelector(".cev14").innerHTML = "تسوق الآن!";
+document.querySelector(".cev15").innerHTML = "تعكس أسلوبك.";
+document.querySelector(".cev16").innerHTML = "تي شيرت";
+document.querySelector(".cev17").innerHTML = "أساسيات الصيف";
+document.querySelector(".cev18").innerHTML = "فساتين";
+document.querySelector(".cev19").innerHTML = "مريح وأنيق.";
+document.querySelector(".cev20").innerHTML = "سبور";
+document.querySelector(".cev21").innerHTML = "يناسب الاسلوب";
+document.querySelector(".cev22").innerHTML = "بلوزة";
+document.querySelector(".cev23").innerHTML = "دائما في الاسلوب.";
+document.querySelector(".cev24").innerHTML = "فساتين السهرة";
+document.querySelector(".cev25").innerHTML = "حتى في النهار.";
+document.querySelector(".cev26").innerHTML = "معلومات عنا";
+document.querySelector(".cev27").innerHTML = "هل ترغب في بيع ملابس نسائية بأرخص وأحدث صيحات الموضة بالجملة عبر الإنترنت في الولايات المتحدة الأمريكية أو أوروبا أو أكثر من 100 دولة أخرى؟ سواء كنت متجرًا بوتيكًا أو بائع تجزئة عبر الإنترنت ، من كلاسيكي إلى عصري ، رسمي إلى غير رسمي ، فإن أكثر من 50000 من أنماط الملابس النسائية العصرية والفريدة من نوعها في جينيس ستكون خيارك الأول للشراء بالجملة! كأفضل بائع ، نقوم بالجملة بأحدث الملابس النسائية الأنيقة المناسبة لأي موسم أو مناسبة. عندما يتعلق الأمر بالصيف ، ما هو أكثر ما يتم الحديث عنه في سوق الملابس النسائية؟ ملابس السباحة مثير لا غنى عنها. البيكينيات ، البيكينيات ، وحتى ملابس السباحة ذات المقاسات الكبيرة ، كلها متاحة على مواقعنا الإلكترونية. جميع التحديثات اليومية لملابس البحر وملابس السباحة والأنماط والألوان والأحجام الشائعة سترضي عملك. فستان مزخرف بالزهور أو فستان القميص مناسب أيضًا لشغف الصيف. إذا كان عميلك بحاجة إلى حضور مناسبة خاصة ، فقدم له فستان سهرة أنيقًا أو فستان حفلة أنيق. بالإضافة إلى ذلك ، تحظى الملابس النسائية غير الرسمية بشعبية كبيرة أيضًا. كل من بلوزات Puffy Sleeve النسائية ، وبلوزات Business Boutique ، والقميص المطبوع ، وحمالة صدر أو شورت رياضي نسائي من القماش الشفاف ، يمكنك بيع أحدث الأساليب في Janes one-stop بالجملة.";
+document.querySelector(".cev28").innerHTML = "فئات";
+document.querySelector(".cev29").innerHTML = "بلوزة";
+document.querySelector(".cev30").innerHTML = "بنطال";
+document.querySelector(".cev31").innerHTML = "فساتين";
+document.querySelector(".cev32").innerHTML = "سبور";
+document.querySelector(".cev33").innerHTML = "الروابط";
 
+
+
+
+
+}
+
+// let xml
+// fetch("http://modayakamoz.com/xml/sena")
+// .then(res=>res.text())
+// .then(data=>{
+//   let parser = new DOMParser();
+//   xml = parser.parseFromString(data, "application/xml");
+
+//   xml = xml.getElementsByTagName("Urun");
+//   console.log(xml);
+// });
